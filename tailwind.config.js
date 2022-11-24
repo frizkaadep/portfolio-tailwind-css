521526c77b4ad201b12dc01ddd8834fc02198cff
Frizka Ade P <106709088+frizkaadep@users.noreply.github.com>
2022-11-24 15:19:26 +0700

    Hero Section
    tools :
    1. https://www.remove.bg/
    2. https://www.blobmaker.app/

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["index.html"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      colors: {
+        primary: "#14b8a6",
+        dark: "#0f172a",
+      },
+      screens: {
+        "2xl": "1320",
+      },
+    },
   },
   plugins: [],
 };
